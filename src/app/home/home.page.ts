3f7b26115a586483fd3192df9c5cbbf988af3530
Hunter Jenkins <hunterbjenkins@gmail.com>
2020-05-04 20:07:46 -0600

    test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor() {}
+
+  public innerWidth: any;
+  ngOnInit() {
+      this.innerWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+    console.log(this.innerWidth);
+  }
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView();
+}
 
 }
